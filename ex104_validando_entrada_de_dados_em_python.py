7f06dc7890088e9acc7a842faf7aae1dfb44ecec
joaorochapy <joaorocha.ar@gmail.com>
2022-03-24 20:17:55 -0300

    exercicio 104

--- ex104_validando_entrada_de_dados_em_python.py
+++ ex104_validando_entrada_de_dados_em_python.py
@@ -1,22 +1,42 @@
 def leiaInt(msg):
 	ok = False
-	valor = 0
 	
 	while True:
 		n = str(input(msg))
 		
 		if n.isnumeric():
-			valor = int(n)
+			n = int(n)
 			ok = True
 			
-		else:
-			print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
-			
-		if ok:
 			break
 			
-	return valor
-		
-		
+		else:
+			print('ERRO! Digite um número inteiro válido.')
+			
+	return n
+	
+	
 n = leiaInt('Digite um número: ')
 print(f'Você digitou o número {n}.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
