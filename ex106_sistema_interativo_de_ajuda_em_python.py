71f9d05930a26b34a5dd391acdf9686a661161c4
joaorochapy <joaorocha.ar@gmail.com>
2022-04-08 21:01:14 -0300

    ex106 def cores

--- ex106_sistema_interativo_de_ajuda_em_python.py
+++ ex106_sistema_interativo_de_ajuda_em_python.py
@@ -1,23 +1,44 @@
 from time import sleep
 
+c = [
+	'\033[0;0m',  # 0 --> Sem cor
+	'\033[1;44m', # 1 --> Azul
+	'\033[1;42m', # 2 --> Verde
+	'\033[1;41m'  # 3 --> Vermelho
+]
+
 def ajuda(com):
+	sleep(0.5)
+
+	cores(f'Buscando o manual da biblioteca ou função {com}', 1)
+
+	sleep(1)
+
 	help(com)
 
 
-# Programa principal
-while True:
-	print('\033[1;42mBem-vindo(a) à ajuda intertiva!')
+def cores(msg, cor=0):
+	tam = len(msg)
 
-	comando = str(input('\033[0;0mDigite uma biblioteca ou comando: '))
+	print(c[cor])
+	print('=' * tam)
+	print(f'{msg}')
+	print('=' * tam)
+	print(c[0])
+
+
+# Programa principal
+cores('Bem-vindo(a) à ajuda intertiva!', 2)
+
+while True:
+	comando = str(input('Digite uma biblioteca ou função: '))
 	
 	if comando.upper() == 'SAIR':
+		sleep(0.5)
+
+		cores('GOOD BYE', 3)
+		sleep(0.5)
 		break
 		
 	else:
-		sleep(0.5)
-
-		print(f'\033[1;44mBuscando as informações da biblioteca/comando {comando}\033[0;0m')
-		
-		sleep(1)
-
 		ajuda(comando)
